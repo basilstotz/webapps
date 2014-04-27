cbed30aac083bc69249bb2ab04243a21b6a69d93
Basil Stotz <basil.stotz@gmail.com>
2014-04-27 14:48:58 +0200

    initial

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export EMAIL="stotz@amxa.ch"
+export DEBFULLNAME="Basil Stotz"
+
+NAME="webapps-1.0"
+
+mkdir $NAME
+cd $NAME
+
+dh_make --createorig --indep
+#mkdir essentials
+#mv debian/{changelog,compat,rules,control} essentials
+#rm -r debian
+#mv essentials debian
+
