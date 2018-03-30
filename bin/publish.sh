6b229d3d6f1d3c4e1d186a0c1dcfc8dc686aa2ba
Basil Stotz <basil.stotz@gmail.com>
2018-03-30 17:18:27 +0200

    new publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,5 +6,6 @@ echo
 echo "copying..."
 scp $DEB root@archive.amxa.ch:/tmp/$DEB
 echo "including..."
-ssh root@archive.amxa.ch includedeb ubuntu trusty $DEB
+#ssh root@archive.amxa.ch includedeb ubuntu trusty $DEB
+ssh root@archive.amxa.ch reprepro -b /srv/apt/debian includedeb trusty /tmp/$DEB \; rm /tmp/$DEB
 echo "fertig"
